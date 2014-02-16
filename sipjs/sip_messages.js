966d4f9442cd24166c56a72bc7788b2492dd254e
genjinoguchi <genji.noguchi@gmail.com>
2014-02-16 01:00:50 -0500

    Added experimental sip

---
+++ sipjs/sip_messages.js
@@ -0,0 +1,19 @@
+var message;
+var
+
+function register() {
+	userAgent.start();
+
+	document.getElementb=ById('send').addEventListener("click", function()) {
+		userAgent.message("messages@codeday.onsip.com")
+	}
+
+	conf = new SIP.UA
+}
+
+function read_message(){
+	var conf = new SIP.UA('messages@codeday.onsip.com').
+		on('message', function (message)){
+			
+		}
+}
