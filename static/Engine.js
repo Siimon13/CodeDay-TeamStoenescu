622f378f066682158e0b4e847ae36511fae3fcf0
Victor Jiao <VjiaoBlack@gmail.com>
2014-02-16 06:38:14 -0500

    fixed error

--- static/Engine.js
+++ static/Engine.js
@@ -56,8 +56,6 @@ var play_sound = function(num){
 		var sound = "twig";
 	}
 	var list = room_that_hear_sound.push(sound);
-	sendmessage(list); // should send message
-
 	if($.contains(current_player.currentRoom.name, room_that_hear_sound) != -1 && num != 0){
 		// either twig or scream....
 		if (current_player.currentRoom.name == "bedroom") {
@@ -69,6 +67,7 @@ var play_sound = function(num){
 			audio.play();
 		}
 	}
+	sendmessage(list); // should send message
 
 
 	//document.getElementById("myDiv").innerHTML = document.getElementById("myDiv").innerHTML + room_that_hear_sound;
